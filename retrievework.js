325dd541ef0c21e24962860d9b60463c8324634e
Herbert Ssevume <you@example.com>
2022-05-08 22:04:50 +0100

    ver03

--- retrievework.js
+++ retrievework.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const Metalworks = require('./model/upload')
 
 router.get("/retrievework", async(req, res)=>{
-    console.log(req.query.category) 
+    //console.log(req.query.category) 
     const metalworks = await Metalworks.find({category:req.query.category})
     
     res.send(metalworks)
